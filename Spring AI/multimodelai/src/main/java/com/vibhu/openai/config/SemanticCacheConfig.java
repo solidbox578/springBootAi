605baf78a5eaff34377c94dc456487ff0bada85f
solidbox578 <bhushanbox@gmail.com>
2026-07-30 21:39:54 +0530

    SemanticCache with Qdrant, not working as expected. Need to check later

--- Spring AI/multimodelai/src/main/java/com/vibhu/openai/config/SemanticCacheConfig.java
+++ Spring AI/multimodelai/src/main/java/com/vibhu/openai/config/SemanticCacheConfig.java
@@ -1,10 +1,14 @@
 package com.vibhu.openai.config;
 
+import io.qdrant.client.QdrantClient;
 import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.ai.chat.cache.semantic.SemanticCache;
 import org.springframework.ai.chat.cache.semantic.SemanticCacheAdvisor;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
 import org.springframework.ai.vectorstore.redis.cache.semantic.DefaultSemanticCache;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.*;
@@ -12,7 +16,8 @@ import redis.clients.jedis.*;
 @Configuration
 public class SemanticCacheConfig {
 
-    @Bean
+    /* Given below Bean configuration is for redis semantic cache, where we need separate redis infrastructure */
+    /*@Bean
     public RedisClient redisClient() {
         return RedisClient.builder().hostAndPort("localhost", 6379).build();
     }
@@ -25,6 +30,27 @@ public class SemanticCacheConfig {
                 .embeddingModel(embeddingModel)
                 .distanceThreshold(0.3)  //Similarity = 1 - Distance, so here similarityThreshold would be 1-0.3 = 0.7
                 .indexName("my-cache")
+                .prefix("chat:")
+                .build();
+    }*/
+
+    /*Given below bean configuration is for Qdrant based Semantic Cache. Since we're already using Qdrant for RAG vector db,
+    we don't need to separate cache infrastructure */
+    @Bean("cacheVectorStore")
+    VectorStore cacheVectorStore(QdrantClient  qdrantClient, EmbeddingModel embeddingModel) {
+        return QdrantVectorStore.builder(qdrantClient, embeddingModel)
+                .collectionName("semantic-cache")
+                .initializeSchema(true)
+                .build();
+    }
+
+    @Bean
+    SemanticCache semanticCache(EmbeddingModel embeddingModel, @Qualifier("cacheVectorStore") VectorStore vectorStore) {
+        return DefaultSemanticCache.builder()
+                .vectorStore(vectorStore)
+                .embeddingModel(embeddingModel)
+                .similarityThreshold(0.9)
+                //.distanceThreshold(0.1)  // Similarity = 1 - Distance, so here similarityThreshold would be 1-0.3 = 0.7
                 .prefix("chat:")
                 .build();
     }
